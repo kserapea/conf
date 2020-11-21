82954a6ef54bfe6e9271d64fde27ca43e43b0913
choct155 <choct155@gmail.com>
2020-11-21 17:09:35 -0500

    Updating the path in this version of init.el to reflect the system path

--- init.el
+++ init.el
@@ -4,6 +4,17 @@
 (setq user-full-name "Marvin Ward Jr."
       user-mail-address "choct155@gmail.com")
 
+;; Adjust path
+(defun set-exec-path-from-shell-PATH ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
+
+This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+(set-exec-path-from-shell-PATH)
+
 ;; Garbage collection and large file thresholds are way too low
 (setq gc-cons-threshold 50000000)
 (setq large-file-warning-threshold 100000000)
